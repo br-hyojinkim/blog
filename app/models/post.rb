aa58a57539a9b746ded5ee251d66f39ad98743c4
김효진 <hyojin.kim@braincommerce.com>
2022-03-18 15:44:54 +0900

    refactor: README 삭제
    - README 삭제
    - User Destroy시 Post 삭제

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,5 +5,12 @@ class Post < ActiveRecord::Base
   has_many :comments, :dependent => :destroy
   has_many :tags, :dependent => :destroy
 
+  after_destroy :log_destroy_action
+
   accepts_nested_attributes_for :tags, :allow_destroy => true, :reject_if => proc { |attrs| attrs.all? { |k, v| v.blank? } }
+
+  private
+  def log_destroy_action
+    puts 'Posts are also destroyed'
+  end
 end
